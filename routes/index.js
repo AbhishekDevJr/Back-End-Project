b335d2d8b79d384e935063c76da6fb4517014ab2
abhishek <abhishek@finestardiamonds.com>
2023-12-16 18:10:53 +0530

    Initial Structure Setup, GET Methods for root/new routes, POST Method for /new route and handling Methods using callbacks

---
+++ routes/index.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var router = express.Router();
+const moment = require('moment');
+
+const userMessages = [
+  {
+    text: "Hi there. I am the Fastest Shinobi!",
+    user: "Namikaze",
+    added: moment().format('DD/MM/YYYY, HH:mm')
+  },
+  {
+    text: "Hey, I just wanna relax & enjoy life.",
+    user: "Shikamaru",
+    added: moment().format('DD/MM/YYYY, HH:mm')
+  },
+  {
+    text: "Hi, I am him!",
+    user: "Kakashi",
+    added: moment().format('DD/MM/YYYY, HH:mm')
+  }
+];
+
+try {
+  /* GET home page. */
+  router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Legendary MessageBoard', userMessages: userMessages });
+  });
+
+  router.get('/new', (req, res, next) => {
+    res.render('form', { title: 'User Form' });
+  });
+
+  router.post('/new', (req, res, next) => {
+    console.log('Req----->', req.body);
+    userMessages.push({
+      text: req.body.userMessage,
+      user: req.body.userName,
+      added: moment().format('DD/MM/YYYY, HH:mm')
+    });
+
+    res.redirect('/');
+  });
+} catch (e) {
+  console.log('Error------>', e.message);
+}
+
+module.exports = router;
